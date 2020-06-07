c62387572b44c75f4be6345dc659b833c3c03977
syaina <syainanurf@gmail.com>
2020-06-07 20:00:16 +0700

    laravel + guzzle cari wisata

--- Frontend/routes/web.php
+++ Frontend/routes/web.php
@@ -11,6 +11,28 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
+
+// Route::get('/detail', function () {
+//     return view('detail');
+// });
+
+// Route::get('/wisata', function () {
+//     return view('wisata');
+// });
+
+
+Route::get('/', 'WisataController@index')->name('home');
+Route::get('/wisata', 'WisataController@wisata')->name('wisata');
+
+Route::post('/wisata', 'WisataController@cariWisata')->name('cari');
+
+Route::get('/cari-wisata/{nama}/{kategori}/{lokasi}', 'WisataController@hasilWisata')->name('hasil');
+//Route::get('/wisata/cari?{nama}+{kategori}+{lokasi}', array('uses' => 'WisataController@hasilWisata'))->name('hasil');
+
+Route::get('/wisata/{id}', 'WisataController@detail')->name('detail');
+
+
+Route::post('/searchkondisi','WisataController@searchkondisi');
